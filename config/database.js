2a61d1be15a90ca9a5b9efafaf74796b49ea6347
Daniel Sanchez <dsan@mit.edu>
2014-11-18 17:59:46 -0500

    changed mongo pointer for openshift

--- config/database.js
+++ config/database.js
@@ -1,5 +1,15 @@
 // config/database.js
+
+var connection_string = 'localhost:27017/EmployTest';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/EmployTest';
+}
+
 module.exports = {
   // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	'url' : 'mongodb://localhost:27017/EmployTest'
+	'url' : 'mongodb://' + connection_string
 };
