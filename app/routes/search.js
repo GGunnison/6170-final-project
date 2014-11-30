3240802fcf2e9a75b22c8fba38eee3abac23f63c
sdrammis <sdrammis@mit.edu>
2014-11-30 17:43:06 -0500

    search route file

---
+++ app/routes/search.js
@@ -0,0 +1,13 @@
+// Authors: Sabrian Drammis
+var router   = require('express').Router();
+var utils    = require('../utils/utils.js');
+var assert   = require('assert');
+var mongoose = require('mongoose');
+
+// database models
+var Student  = require('../models/StudentModel');
+var Employer = require('../models/EmployerModel');
+
+router.get('/', function (req, res) {
+  // render the search view here
+});
