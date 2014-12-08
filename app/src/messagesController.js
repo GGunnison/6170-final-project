074ca08a40f96929915093232148c45ad07f7611
Grant Gunnison <gunnison@mit.edu>
2014-12-08 02:37:12 -0500

    more messaging

--- app/src/messagesController.js
+++ app/src/messagesController.js
@@ -42,7 +42,7 @@ var MessagesController = function() {
     eventListeners();
 
     //Initialize to have the inbox up to start
-    helpers.renderInbox();
+    $('#inbox').click();
 
     $('.alert').hide();
   }
@@ -53,6 +53,10 @@ var MessagesController = function() {
         type: "GET",
         url: '/messages/inbox',
         success: function (data) {
+          $('#inbox').addClass('selected');
+          $('#sent').removeClass('selected');
+          $('#create').removeClass('selected');
+
           helpers.renderInbox(data);
         }
       });
@@ -63,14 +67,51 @@ var MessagesController = function() {
         type: "GET",
         url: '/messages/sentbox',
         success: function(data) {
+          $('#inbox').removeClass('selected');
+          $('#sent').addClass('selected');
+          $('#create').removeClass('selected');
           helpers.renderSent(data);
         }
       });
     });
 
-    $('create').on('click', function() {
+    $('#create').on('click', function() {
+      $('#inbox').removeClass('selected');
+      $('#sent').removeClass('selected');
+      $('#create').addClass('selected');
       helpers.renderCreate();
     });
+
+    // $('#delete').on('click', function(){
+    //   console.log("hello");
+    //   console.log($('delete').parent.name);
+    //   // $.ajax({
+    //   //   type:"DELETE",
+    //   //   url: '/'+
+    //   // })
+    // });
+
+    $(document).on('click', '#delete', function(e) {
+      e.stopPropagation();
+      // $ajax({
+      //   type: "DELETE",
+      //   url: '/' +
+      // })
+      var id = $(this).parent().attr("id");
+      console.log(id);
+      // console.log($('#delete').parent.parent.parent.parent.parent);
+
+    });
+
+
+//     $('#myAffix').affix({
+//       offset: {
+//       top: 100,
+//       bottom: function () {
+//         return (this.bottom = $('.footer').outerHeight(true))
+//     }
+//   }
+// })
 
     $(document).on('click', '.panel-body', function() {
         $(this).children('.rest').slideToggle();
