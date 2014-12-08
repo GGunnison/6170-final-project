6895d2862401e2c276e062fb9dbe3f3c90eaa268
sdrammis <sdrammis@mit.edu>
2014-12-08 03:37:45 -0500

    more stuff for modal, getting info with jquery

--- app/src/messageCreationController.js
+++ app/src/messageCreationController.js
@@ -36,8 +36,19 @@ var MessageCreationController = function() {
   var eventListeners = function() {
     $(document).on('click', '.createMessageBtn', function () {
       helpers.toggleModal();
+
       $('.modal-title').text('To:  ' + $(this).attr('name'));
       //$('.content').fadeToggle();
+    });
+
+    $(document).on('click', '#sendMessage', function () {
+      var toName  = $('.createMessageBtn').attr('name');
+      var title   = $('#title').val();
+      var message = $('#message').val();
+
+      console.log(toName);
+      console.log(title);
+      console.log(message);
     });
   }
 
